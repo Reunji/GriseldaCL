a4ed77acbb95ece6b3535654f929ad44a17063a5
Reunji <55166669+Reunji@users.noreply.github.com>
2020-02-06 18:44:14 -0500

    First Check-in.
    Griselda, real rap for real rap listeners.

---
+++ Android/app/src/main/java/com/example/griseldacl/GriseldaMember.java
@@ -0,0 +1,9 @@
+package com.example.griseldacl;
+
+public class GriseldaMember extends Rapper {
+
+    public GriseldaMember(){
+        haveBars = true;
+        canSing = false;
+    }
+}
---
+++ Android/app/src/main/java/com/example/griseldacl/Rapper.java
@@ -0,0 +1,6 @@
+package com.example.griseldacl;
+
+public class Rapper {
+    public boolean haveBars;
+    public boolean canSing;
+}
